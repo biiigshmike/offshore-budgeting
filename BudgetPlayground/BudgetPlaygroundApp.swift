8a2bc4cf1adc66393c8937365b16a2033d950976
Michael <brownmlee@icloud.com>
2026-01-20 16:15:58 -0800

    Initial Commit

---
+++ BudgetPlayground/BudgetPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetPlaygroundApp.swift
+//  BudgetPlayground
+//
+//  Created by Michael Brown on 1/20/26.
+//
+
+import SwiftUI
+
+@main
+struct BudgetPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BudgetPlaygroundTests/BudgetPlaygroundTests.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetPlaygroundTests.swift
+//  BudgetPlaygroundTests
+//
+//  Created by Michael Brown on 1/20/26.
+//
+
+import Testing
+@testable import BudgetPlayground
+
+struct BudgetPlaygroundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
